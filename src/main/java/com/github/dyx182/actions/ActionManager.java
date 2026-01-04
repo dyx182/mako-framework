8e7447a1e5228da9c3b695634868b9439ad9976c
dyx182 <keyf8797@mail.ru>
2026-01-04 20:21:36 +0400

    added ActionManager

---
+++ src/main/java/com/github/dyx182/actions/ActionManager.java
@@ -0,0 +1,34 @@
+package com.github.dyx182.actions;
+
+import com.github.dyx182.actions.impl.ClickAction;
+import com.github.dyx182.actions.impl.SetAction;
+import com.github.dyx182.core.LocatorManager;
+import lombok.AllArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@AllArgsConstructor
+public class ActionManager {
+
+    private final Map<String, TestAction> actionsRegistry = new HashMap<>();
+    private final LocatorManager locatorManager;
+
+    private void registerActions() {
+        register(new ClickAction(locatorManager));
+        register(new SetAction(locatorManager));
+    }
+
+    public void register(TestAction action) {
+        actionsRegistry.put(action.getName(), action);
+        System.out.println("Registered action - " + action.getName());
+    }
+
+    public TestAction getAction(String actionName) {
+        TestAction action = actionsRegistry.get(actionName);
+        if(action==null) {
+            throw new IllegalArgumentException("Unknown action");
+        }
+        return action;
+    }
+}
